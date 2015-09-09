554e703f9548b2846b0efb6cff81d8dc52759d9d
Toan <jerrytran3011@gmail.com>
2015-09-09 14:33:10 -0400

    Assignment 1

---
+++ Practice Programs/src/Assignment.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter two numbers to multiply.");
+		
+		 int n1, n2;
+		 
+		 @SuppressWarnings("resource")
+		Scanner keyboard = new Scanner(System.in);
+		 n1 = keyboard.nextInt();
+		 n2 = keyboard.nextInt();
+		
+		System.out.println("The product is");
+	    System.out.println(n1 * n2);
+
+	}
+
+}
